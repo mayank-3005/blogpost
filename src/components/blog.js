dd19bc0cce2714904e04c9c6f8fcfcd440cf437c
mayank <chandravanshimayank@gmail.com>
2024-04-13 21:39:58 +0530

    change

--- src/components/blog.js
+++ src/components/blog.js
@@ -3,8 +3,26 @@ import React from 'react';
 const Blog = ({ post }) => {
   return (
     <div className="blog-post">
-      <h3>{post.title}</h3>
+    <div class="container">
+  <div class="card">
+    <div class="card__header">
+      <img src= {post.image} alt="card__image" class="card__image" width="600"></img>
+    </div>
+    <div class="card__body">
+      <h4>{post.title}</h4>
       <p>{post.content.substring(0, 100)}...</p>
+    </div>
+    <div class="card__footer">
+      <div class="user">
+        <div class="user__info">
+          <h5>{post.author}</h5>
+          <small>Date :{post.date} </small>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
     </div>
   );
 };
--- src/components/blogDetailspage.js
+++ src/components/blogDetailspage.js
@@ -1,11 +1,17 @@
-// BlogDetails.js
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BlogDetails = ({ post }) => {
   return (
     <div className='blog-detalis-page'>
+    <img src= {post.image} alt="card__image" class="card__image" width="600"></img>
       <h2>{post.title}</h2>
       <p>{post.content}</p>
+      <button className='back-btn'>
+      <Link to='/'> Back to blog post</Link>
+      </button>
+      
     </div>
   );
 };
